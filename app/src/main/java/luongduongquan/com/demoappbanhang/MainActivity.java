d70371d0288ffd1024e4c7a58234c198ea81891c
dquand91 <dquand91@gmail.com>
2018-08-20 11:36:09 +0700

    - Add DrawerLayout and DrawerToggle.

--- app/src/main/java/luongduongquan/com/demoappbanhang/MainActivity.java
+++ app/src/main/java/luongduongquan/com/demoappbanhang/MainActivity.java
@@ -4,6 +4,8 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -18,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
 	private ViewPager mViewPager;
 	private ViewPagerAdapterMain mViewPagerAdapter;
 
+	private DrawerLayout mDrawerLayout;
+	private ActionBarDrawerToggle mDrawerToggle; // cái dấu 3 gạch để bấm vào sẽ show ra Drawer Layout
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -30,6 +35,13 @@ public class MainActivity extends AppCompatActivity {
 
 		toolbar.setTitle("");
 		setSupportActionBar(toolbar);
+		getSupportActionBar().setHomeButtonEnabled(true);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+		mDrawerLayout = findViewById(R.id.drawerLayout_main);
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,  R.string.open_drawer, R.string.close_drawer);
+		mDrawerLayout.addDrawerListener(mDrawerToggle); // gán cái toggle cho DrawerLayout, để khi bấm vào Toggle sẽ show ra cái DrawerLayout.
+		mDrawerToggle.syncState();
 
 		mViewPagerAdapter = new ViewPagerAdapterMain(getSupportFragmentManager());
 		mViewPager.setAdapter(mViewPagerAdapter);
@@ -48,6 +60,9 @@ public class MainActivity extends AppCompatActivity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+		if(mDrawerToggle.onOptionsItemSelected(item)){
+			return true;
+		}
 		return super.onOptionsItemSelected(item);
 	}
 }
